4c61cace4839ccac0ff4cb707f4798f22ebbc51f
Maldon <maldonmeehan@gmail.com>
2016-08-19 13:06:51 -0700

    add working update, add placeholders and remove update button from home page

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -3,11 +3,3 @@ import Ember from 'ember';
 export default Ember.Component.extend({
 
 });
-
-
-// updateQuestionForm: false,
-// actions: {
-//   update(question, params) {
-//     this.sendAction('update', question, params);
-//   }
-// }
