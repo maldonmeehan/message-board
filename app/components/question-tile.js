e1fd8a09936e4c585ca8a8c3362d267f4007ff08
Maldon <maldonmeehan@gmail.com>
2016-08-19 12:31:18 -0700

    refactor question detail and update to take place on indivual quesiton page

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -1,10 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  updateQuestionForm: false,
-  actions: {
-    update(question, params) {
-      this.sendAction('update', question, params);
-    }
-  }
+
 });
+
+
+// updateQuestionForm: false,
+// actions: {
+//   update(question, params) {
+//     this.sendAction('update', question, params);
+//   }
+// }
