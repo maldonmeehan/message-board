c4d38b2aab0d3d1403f0026dd00467956aa3902e
Maldon <maldonmeehan@gmail.com>
2016-08-18 18:55:26 -0700

    add question component and display each questions details

--- app/models/question.js
+++ app/models/question.js
@@ -2,7 +2,8 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   name: DS.attr(),
-  content: DS.attr()
+  content: DS.attr(),
+  note: DS.attr()
 });
 
 // This file is essentially a 'blueprint' listing out the attributes each of our rentals should contain. The DS in this file stands for "data store". .attr() stands for 'attribute'. This is a list of object attributes we want to save to our store for each question we add. When a DS.Model is exported, as we see in the line export default DS.Model.extend({ ... it is added to the data store. That is, it's being saved to our database.
