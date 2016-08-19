9a4d8dceeab79bd0b8118bb32a68a3bd10f11e4c
Maldon <maldonmeehan@gmail.com>
2016-08-18 18:12:09 -0700

    add model hook, hard-coded data,to index js route and an handlebar helper to loop through each object in the model in the corresponding index hbs template

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,24 @@
 import Ember from 'ember';
 
+var questions = [{
+  id: 1,
+  name: "Maldon Meehan",
+  question: "Her recording career lasted just eight years, starting in 1955 with A Church, A Courtroom And Then Good-Bye."
+  //patsy cline
+}, {
+  id: 2,
+  name: "Alex Trebek",
+  question: "Other than Q, these two letters appear the least in the names of states, each appearing only once."
+  //J and z
+}, {
+  id: 3,
+  name: "Alex Trebek",
+  question: "One of the top 20, this 1946 film was based on a short story published as The Man Who Was Never Born."
+  //It's a wonderful life
+}];
+
 export default Ember.Route.extend({
+  model() {  //model hook returns a hard-coded array of questions
+    return questions;
+  },
 });
