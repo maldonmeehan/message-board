949b0387213a291cfc8641d2e3ac35c3a0a7d760
Maldon <maldonmeehan@gmail.com>
2016-08-22 07:52:47 -0700

    remove this.get ?

--- app/components/new-answer.js
+++ app/components/new-answer.js
@@ -8,8 +8,8 @@ export default Ember.Component.extend({
     },
     saveAnswer() {
       var params = {
-        name: this.get('name') ? this.get('name'):"",
-        content: this.get('content') ? this.get('content'):"",
+        name: this.get('name'),
+        content: this.get('content'),
         question: this.get('question')
       };
       this.set('addNewAnswer', false);
