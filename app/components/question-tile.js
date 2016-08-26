53703814bfb93aa0a30b663248d97079728c28a2
maldonmeehan <maldonmeehan@gmail.com>
2016-08-26 13:39:55 -0700

    add service for favorites not working yet

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -1,14 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  favoriteCart: Ember.inject.service(),
-
-  actions: {
-      addToCart(question) {
-        this.get('favoriteCart').add(question);
-      },
+  favoriteQuestions: Ember.inject.service(),
 
   fullQuestion: Ember.computed('question.name', 'question.content', function() {
   return this.get('question.name') + ' wants to know ' + this.get('question.content');
   }),
+
+  actions: {
+      addToFavorite(question) {
+        debugger;
+        this.get('favoriteQuestions').add(question);
+      }
+    }
 });
