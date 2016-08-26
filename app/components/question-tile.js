4752ffa165d3407071e3077c97ea92512a4d8a6f
maldonmeehan <maldonmeehan@gmail.com>
2016-08-26 10:18:31 -0700

    add computed property to combine both the name and question with fullQuestion

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -1,5 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  fullQuestion: Ember.computed('question.name', 'question.content', function() {
+  return this.get('question.name') + ' wants to know ' + this.get('question.content');
+}),
 
 });
