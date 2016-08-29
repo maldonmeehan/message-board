02b1951ce6fd6b49ba1ebeffa828812647882df5
maldon <maldonmeehan@gmail.com>
2016-08-29 07:02:12 -0700

    add computed property to sort favorite questions by points

--- app/components/display-favorites.js
+++ app/components/display-favorites.js
@@ -2,4 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
     favoriteQuestions: Ember.inject.service(),
+
+    sortBy: ['point:asc'],
+    sortedFavoriteQuestions: Ember.computed.sort('favoriteQuestions.questions', 'sortBy'),
 });
